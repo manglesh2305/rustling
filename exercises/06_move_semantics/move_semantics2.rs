4d244c0c1e9f6d6594a6f14bf18a2148446caf1c
Manglesh Yadav <mangleshyadav5456@gmail.com>
2025-09-06 19:51:39 +0530

    Completed Vector & Move Semantics

--- exercises/06_move_semantics/move_semantics2.rs
+++ exercises/06_move_semantics/move_semantics2.rs
@@ -19,8 +19,12 @@ mod tests {
     #[test]
     fn move_semantics2() {
         let vec0 = vec![22, 44, 66];
+        let mut v: Vec<i32> = Vec::new();
+        for element in &vec0{
+            v.push(*element)
+        }
 
-        let vec1 = fill_vec(vec0);
+        let vec1 = fill_vec(v);
 
         assert_eq!(vec0, [22, 44, 66]);
         assert_eq!(vec1, [22, 44, 66, 88]);
